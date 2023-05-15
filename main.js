6e953006c88dfa559cc8c23e92d44f643b361925
Newbie-Alert <133559761+Newbie-Alert@users.noreply.github.com>
2023-05-15 22:33:58 +0900

    add music, add 3d & 2d function,  add button
    add music - 성시경_태양계
    3D function - 태양계 보르텍스 움직임이 떠올라서 추가함.
    2D function - 기본적은 2D 형태로 되돌리는 것.
    각각의 행성의 이름과 투명도를 결정하는 버튼 추가.

---
+++ main.js
@@ -0,0 +1,77 @@
+// solar system
+const btns = document.querySelectorAll('.btn h6');
+const sun = document.querySelector('#sun');
+const planets = document.querySelectorAll('.line');
+const names = document.querySelectorAll('.name');
+const background = document.querySelector('#solar-system');
+const all = document.querySelector('.all-btn');
+const bortex = document.querySelector('.threeD-btn');
+const normal = document.querySelector('.twoD-btn');
+const zoom = document.querySelector('.zoom-out')
+
+
+// 3D
+bortex.addEventListener('click', () => {
+  planets.forEach(el => {
+    el.style.animation = `bgRotate 30s linear infinite`;
+  })
+  zoom.zIndex = 1;
+  zoom.style.opacity = 1;
+  setTimeout(zoomOut, 3000)
+})
+function zoomOut() {
+  zoom.style.opacity = 0;
+}
+
+//2D
+normal.addEventListener('click', () => {
+  planets.forEach(el => {
+    el.style.animation = ``;
+  })
+})
+
+
+
+all.addEventListener('click', () => {
+  showAll();
+})
+
+background.addEventListener('click', (e) => {
+  e.stopPropagation();
+  hideName();
+  opacityLow();
+})
+
+btns.forEach(el => {
+  el.addEventListener('click', (e) => {
+    let index = e.target.dataset.id;
+    hideName();
+    names[index].style.display = 'block';
+
+    if (index == index) {
+      opacityLow();
+      planets[index].style.opacity = 1;
+    }
+  })
+})
+
+function hideName() {
+  names.forEach(item => {
+    item.style.display = 'none';
+  })
+}
+function showAll() {
+  names.forEach(item => {
+    item.style.display = 'block'
+  })
+  planets.forEach(item => {
+    item.style.opacity = 1;
+  })
+}
+
+function opacityLow() {
+  planets.forEach(item => {
+    item.style.opacity = 0.5;
+  }
+  )
+}
